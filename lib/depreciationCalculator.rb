672b22f278bd4ef27c4a8e8b20114432399e14f0
DESKTOP-83C18M7\Mukul Gopinath <x17123739@student.ncirl.ie>
2018-04-22 11:07:51 +0100

    Initial Commit

---
+++ lib/depreciationCalculator.rb
@@ -0,0 +1,12 @@
+require "depreciationCalculator/version"
+
+module DepreciationCalculator
+	def self.calculateDepreciatedValue(carDetails)
+		age_dep = [100,75,65,60,50,45,35,28,22,20,18,15,12,10,8,8,7,7,6,6,5]
+		kms_dep = [100,80,75,70,67,65,60,55,53,50,48,44,40,36,34,30,28,26,24,22,20,19,18,17,16,15,14,13,12,11,10]
+		tyre_cond = [100,60,30]
+		owners = [100,80,75,70,65]
+		@estimated_value = ((age_dep[carDetails['age']] * 0.5) + (kms_dep[carDetails['mileage']] * 0.3) + (tyre_cond[carDetails['tyre']] * 0.05) + (owners[carDetails['owner']] * 0.15)) * carDetails['price'] /100
+		return @estimated_value
+	end
+end
---
+++ lib/depreciationCalculator/version.rb
@@ -0,0 +1,3 @@
+module DepreciationCalculator
+  VERSION = "0.1.1"
+end
